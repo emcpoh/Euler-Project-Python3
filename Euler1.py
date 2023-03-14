6ec7b3437996dcd0784111be7aade60dd596c47d
emcpoh <kaplienkosergei@gmail.com>
2023-03-14 16:22:06 +0300

    Input check and newsum func

--- Euler1.py
+++ Euler1.py
@@ -1,13 +1,18 @@
-N = int(input("Введите число от 0 до 10000: "))
-if 0 <= N <= 10000:
+def newsum(n:int):
     summa = 0
-    for x in range(0, N):
+    for x in range(0, n):
         if x % 3 == 0:
             summa += x
         elif x % 5 == 0:
             summa += x
-    print("Сумма всех чисел меньше ", N, " кратных 3 или 5: ", summa)
-else:
-    N = int(input("Введите число от 0 до 10000: "))
+    print("Сумма всех чисел меньше ", n, " кратных 3 или 5: ", summa)
+num = int(input("Введите число от 0 до 10000: "))
+flag = False
+while flag != True:
+    if 0 <= num <= 10000:
+        newsum(num)
+        flag = True
+    else:
+        num = int(input("Вы указали неверное число, попробуйте снова:  "))
 
 
