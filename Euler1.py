27a89edcc550bbedc084883d82ea2c9d4d1912f4
emcpoh <kaplienkosergei@gmail.com>
2023-03-14 16:13:47 +0300

    Input check

--- Euler1.py
+++ Euler1.py
@@ -1,9 +1,13 @@
-N = input("Введите число от 0 до 10000: ")
-summa = 0
-for x in range(0, N):
-    if x % 3 == 0:
-        summa += x
-    elif x % 5 == 0:
-        summa += x
-print("Сумма всех чисел меньше ", N , " кратных 3 или 5: ", summa)
-#Комментарий для проверки работы Git
+N = int(input("Введите число от 0 до 10000: "))
+if 0 <= N <= 10000:
+    summa = 0
+    for x in range(0, N):
+        if x % 3 == 0:
+            summa += x
+        elif x % 5 == 0:
+            summa += x
+    print("Сумма всех чисел меньше ", N, " кратных 3 или 5: ", summa)
+else:
+    N = int(input("Введите число от 0 до 10000: "))
+
+
