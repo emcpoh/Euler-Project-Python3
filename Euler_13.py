76d858a13aec13852feb36f87896483f59244276
emcpoh <127853217+emcpoh@users.noreply.github.com>
2023-03-17 12:03:03 +0300

    Update and rename Euler13.py to Euler_13.py

--- Euler_13.py
+++ Euler_13.py
@@ -1,6 +1,8 @@
 print('Задание: Найдите первые десять цифр суммы следующих ста 50-значных чисел.'
       '\nЧисла указаны построчно в файле chisla.txt. ')
-file = open('chisla.txt')
+file = open('chisla.txt') #To run the program, you need to place the file chisla.txt in the same directory as the file with the code.
+#File chisla.txt it is located in the repository with the main code. Or, it can be found at the link below:
+#https://github.com/emcpoh/Euler-Project-Python3/blob/cd341fee297ea2097e03a1e6f3a31dfbde82780b/chisla.txt
 summa = 0
 for line in file:
     summa += (int(line))
